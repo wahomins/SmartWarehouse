c31dc61e0dc5ebd87570a3c49fdde79da50645de
wahomins <wahomins@gmail.com>
2023-05-26 07:11:35 +0300

    Added gitignore

--- server.py
+++ server.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
+
+# MongoDB connection details
+mongodb_uri = os.getenv("MONGODB_URI")
 
 # Define routes and middleware here
 
